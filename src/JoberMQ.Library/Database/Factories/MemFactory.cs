325d2e7c0f83e5eaec67759ba142fb044b562ce4
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2023-01-20 08:09:26 +0300

    v0.2.1

--- src/JoberMQ.Library/Database/Factories/MemFactory.cs
+++ src/JoberMQ.Library/Database/Factories/MemFactory.cs
@@ -1,6 +1,7 @@
 ﻿using JoberMQ.Library.Database.Enums;
 using JoberMQ.Library.Database.Repository.Abstraction.Mem;
 using JoberMQ.Library.Database.Repository.Implementation.Mem.Default;
+using System;
 using System.Collections.Concurrent;
 
 namespace JoberMQ.Library.Database.Factories
@@ -9,6 +10,10 @@ namespace JoberMQ.Library.Database.Factories
     {
         public static IMemRepository<TKey, TValue> Create<TKey, TValue>(MemFactoryEnum memFactory, MemDataFactoryEnum memDataFactory, ConcurrentDictionary<TKey, TValue> masterData = null)
         {
+            if (memDataFactory == MemDataFactoryEnum.Data && masterData == null)
+                throw new ArgumentNullException("masterData null");
+
+
             IMemRepository<TKey, TValue> memRepository;
 
             switch (memFactory)
