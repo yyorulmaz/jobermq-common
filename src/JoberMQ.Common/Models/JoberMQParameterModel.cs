c8b3af5bd989dc629b799b27b9e004a7ab8d1bc5
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2023-06-19 02:38:09 +0300

    dd

---
+++ src/JoberMQ.Common/Models/JoberMQParameterModel.cs
@@ -0,0 +1,15 @@
+﻿using JoberMQ.Common.Enums.Endpoint;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace JoberMQ.Common.Models
+{
+    public class JoberMQParameterModel
+    {
+        public string ClientKey { get; set; }
+        public string HostName { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public UrlProtocolEnum UrlProtocol { get; set; }
+        public int Port { get; set; }
+    }
+}
